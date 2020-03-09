7022dc3ba3cb8b8434bb26038946bed1b0736927
sierikov <artem.sierikov@gmail.com>
2020-03-09 15:41:41 +0200

    Task 4 - Added Request Logger

--- task-4/src/main/java/tk/artemser/task4/config/WebConfig.java
+++ task-4/src/main/java/tk/artemser/task4/config/WebConfig.java
@@ -1,14 +1,26 @@
 package tk.artemser.task4.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tk.artemser.task4.converter.StringToEnumConverter;
+import tk.artemser.task4.interceptor.RequestLoggerInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private RequestLoggerInterceptor requestLoggerInterceptor;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggerInterceptor)
+                .addPathPatterns("/**");;
     }
 }
